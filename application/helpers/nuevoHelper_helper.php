bc11b7ccf1e0cb64f7b34231283f04169016d500
David Borge <davidborge@astibot.es>
2018-06-15 14:01:34 +0200

    Nuevo Helper (Tutorial 7)

---
+++ application/helpers/nuevoHelper_helper.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+    * NuevaFuncionEnNuevoHelper
+    *
+    * Nueva función de prueba en \application\helpers\nuevoHelper_helper.php
+    *
+    * @return	string
+    */
+function nuevaFuncionEnNuevoHelper()
+{
+    return "<h1>Estás viendo el return de nuevaFuncionEnNuevoHelper().</h1>";
+}
