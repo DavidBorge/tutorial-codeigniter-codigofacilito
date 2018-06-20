d10efc066a9b34d8009c82905bfbe389a5ad8f3f
David Borge <davidborge@astibot.es>
2018-06-20 11:45:51 +0200

    Versión de CodeIgniter instalada

--- application/views/pruebas/pruebasView.php
+++ application/views/pruebas/pruebasView.php
@@ -1,8 +1,20 @@
 <h1>Pruebas</h1>
 <hr>
 
-<h2>Helpers (Tutorial 7)</h2>
+
+
+<h2>Versión de CodeIgniter instalada</h2>
+<?= CI_VERSION; ?>
+
+
+
+<hr><h2>Helpers (Tutorial 7)</h2>
 <?= nuevaFuncionEnNuevoHelper(); ?>
+
+
 
 <hr><h2>Librerías (Tutorial 8)</h2>
 <?= NuevaLibreria::crearListaUL(array('Elemento 1', 'Elemento 2')); ?>
+
+
+
